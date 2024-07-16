250056451426e4f681cca875608656fec6dae95b
LizaA <atake.liza@gmail.com>
2024-07-16 18:32:12 +0200

    vertex paint
    set up vertex paint
    paint floor meshes

--- addons/vertex_painter/v2/vertex_painter_3d.gd
+++ addons/vertex_painter/v2/vertex_painter_3d.gd
@@ -107,7 +107,7 @@ func start_paint(event: InputEvent) -> void:
 		mesh_i.mesh = ArrayMesh.new()
 		active_mdt.commit_to_surface(mesh_i.mesh)
 		pre_mat = mesh_i.get_surface_override_material(0)
-		mesh_i.set_surface_override_material(0, VERTEX_COLOR)
+		#mesh_i.set_surface_override_material(0, VERTEX_COLOR)
 		process_move(event)
 		click_active = true
 
@@ -122,15 +122,18 @@ func start_paint_legacy(event: InputEvent) -> void:
 		mesh_i.mesh = ArrayMesh.new()
 		active_mdt.commit_to_surface(mesh_i.mesh)
 		pre_mat = mesh_i.get_surface_override_material(0)
-		mesh_i.set_surface_override_material(0, VERTEX_COLOR)
+		#mesh_i.set_surface_override_material(0, VERTEX_COLOR)
 		process_move(event)
 		click_active = true
 
 func stop_paint() -> void:
 	mouse_camera_3d.hide()
 	debug.hide()
+	#if pre_mat == null:
+		#pre_mat = VERTEX_COLOR
+		#mesh_i.set_surface_override_material(0, pre_mat)
 	if pre_mat == null:
-		pre_mat = VERTEX_COLOR
+		pre_mat = mesh_i.get_surface_override_material(0)
 		mesh_i.set_surface_override_material(0, pre_mat)
 	
 	mesh_i.set_meta("_edit_lock_", false)
